633cee687b7663a941a081961c51ab5cb094fed1
Amit Mittal <mittalmailbox@gmail.com>
2018-12-11 18:28:26 +0530

    Typo

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -61,7 +61,7 @@ export default class Header extends Component {
 							<TopAppBar.Icon menu onClick={this.openDrawer}>
 								menu
 							</TopAppBar.Icon>
-							<TopAppBar.Title>Squarefy</TopAppBar.Title>
+							<TopAppBar.Title>Squarify</TopAppBar.Title>
 						</TopAppBar.Section>
 						<TopAppBar.Section
 							align-end
